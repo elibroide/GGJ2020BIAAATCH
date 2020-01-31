809afac6e96b5e3a458be5ac47c987251d5e2e28
Alex Bogdanovsky <alex.bogdanovsky@gmail.com>
2020-01-31 21:26:30 +0200

    AAA

--- Assets/Scripts/AreaOfDig.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public class AreaOfDig : MonoBehaviour
-{
-    public Grave parent;
-}
--- Assets/Scripts/BodyPartType.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-public enum BodyPartType
-{
-    Head,
-    HandLeft,
-    HandRight,
-    LegLeft,
-    LegRight,
-}
