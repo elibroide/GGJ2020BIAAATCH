05271ee0e1c5b87277b3cf294db9f25b0ba495e8
Eli Broide <elibroide@gmail.com>
2020-01-31 10:45:50 +0200

    Added movement

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DG.Tweening;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public PlayerMovementController movementController;
+    public SpriteRenderer sprite;
+
+    private bool isAttacking;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isAttacking)
+        {
+            return;
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            // Perform attack
+            isAttacking = true;
+            movementController.Stop();
+            var sequence = DOTween.Sequence();
+            sequence.InsertCallback(0, () => sprite.color = Color.red);
+            sequence.InsertCallback(2, () => { 
+                isAttacking = false;
+                sprite.color = Color.white;
+            });
+            return;
+        }
+        var direction = Vector2.zero;
+        if (Input.GetKey(KeyCode.A))
+        {
+            direction += Vector2.left;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            direction += Vector2.up;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            direction += Vector2.down;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            direction += Vector2.right;
+        }
+        movementController.SetDirection(direction);
+    }
+
+    private void CheckAttack()
+    {
+        DOTween.Sequence();
+    }
+}
---
+++ Assets/Scripts/Player/PlayerMovementController.cs
@@ -0,0 +1,82 @@
+﻿using DG.Tweening;
+using UnityEngine;
+
+public class PlayerMovementController : MonoBehaviour
+{
+    public float maxSpeed;
+    public float accelerateTime;
+    public float decelerateTime;
+
+    [ReadOnly] public float targetSpeed;
+    [ReadOnly] public float speed;
+    [ReadOnly] public Vector2 direction;
+
+    private float _currentTargetSpeed;
+    private bool _currentIsAccelerating;
+    private Rigidbody2D _rigidbody;
+    private Tween _speedTween;
+    
+    /*
+     *
+     *    Interface
+     * 
+     */
+
+    public void SetDirection(Vector2 direction)
+    {
+        if (direction != Vector2.zero)
+        {
+            targetSpeed = maxSpeed;
+            this.direction = direction;
+        }
+        else
+        {
+            targetSpeed = 0;
+        }
+    }
+
+    public void SetMaxSpeed(float maxSpeed)
+    {
+        this.maxSpeed = maxSpeed;
+    }
+
+    public void Stop()
+    {
+        targetSpeed = 0;
+    }
+    
+    /*
+     *
+     *    Unity
+     * 
+     */
+    
+    // Start is called before the first frame update
+    void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        CalcSpeed();
+        Move();
+    }
+
+    private void CalcSpeed()
+    {
+        if (_currentTargetSpeed == targetSpeed) return;
+        
+        _speedTween?.Kill();
+        var duration = targetSpeed == 0 ? accelerateTime : decelerateTime;
+        _speedTween = DOTween.To(value => speed = value, speed, targetSpeed, duration)
+            .SetTarget(this);
+        _currentTargetSpeed = targetSpeed;
+    }
+
+    private void Move()
+    {
+        _rigidbody.velocity = direction.normalized * (Time.deltaTime * speed);
+    }
+}
