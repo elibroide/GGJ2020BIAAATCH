b4f637076202b592559d24b21754bbb3bb1b82b1
David Rubin <dudrubin@gmail.com>
2020-01-31 11:19:38 +0200

    added doreen,updated gitignore

---
+++ Assets/Scripts/NewMonoBehaviour.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ResourcesManage: MonoBehaviour
+{
+    static GameObject hand;
+    // Use this for initialization
+    void Start()
+    {
+        hand = Resources.Load("hand") as GameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
